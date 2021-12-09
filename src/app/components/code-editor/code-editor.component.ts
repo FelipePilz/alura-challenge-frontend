3239e87c32c90bdd375855f32b3dd78fff10ef40
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-09 01:50:39 -0300

    Melhorado o placeholder do terminal no code-editor

--- src/app/components/code-editor/code-editor.component.ts
+++ src/app/components/code-editor/code-editor.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-code-editor',
   templateUrl: './code-editor.component.html',
-  styleUrls: ['./code-editor.component.scss']
+  styleUrls: ['./code-editor.component.scss'],
 })
-export class CodeEditorComponent implements OnInit {
+export class CodeEditorComponent {
+  public editorPlaceholder = `const pluckDeep = key => obj => key.split('.').reduce((accum, key) => accum[key], obj)
 
-  constructor() { }
+const compose = (...fns) => res => fns.reduce((accum, next) => next(accum), res)
 
-  ngOnInit(): void {
+const unfold = (f, seed) => {
+  const go = (f, seed, acc) => {
+    const res = f(seed)
+    return res ? go(f, res[1], acc.concat([res[0]])) : acc
   }
-
+  return go(f, seed, [])
+}`;
 }
