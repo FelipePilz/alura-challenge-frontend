7f82e8ccd14d08b31e3ae95f61856b3b39577ff9
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-10 13:51:04 -0300

    Implementado sistema de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CodeEditorComponent } from './components/code-editor/code-editor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'editor-de-codigo',
+  },
+  {
+    path: 'editor-de-codigo',
+    component: CodeEditorComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
