ca3c3a411d243578f42a8981f0b3f76eea5c6c7d
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-10 11:09:04 -0300

    Criado componente e modulo do button-filled

---
+++ src/app/components/button/button-filled/button-filled.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button-filled',
+  templateUrl: './button-filled.component.html',
+  styleUrls: ['./button-filled.component.scss'],
+})
+export class ButtonFilledComponent {
+  @Input() mensagem = '';
+}
---
+++ src/app/components/button/button-filled/button-filled.module.ts
@@ -0,0 +1,10 @@
+import { ButtonFilledComponent } from './button-filled.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [ButtonFilledComponent],
+  imports: [CommonModule],
+  exports: [ButtonFilledComponent],
+})
+export class ButtonFilledModule {}
