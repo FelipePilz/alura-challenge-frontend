676f9b88804c48f41b6344836c84a8f0d9291183
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-10 11:29:21 -0300

    Implementado o button-outlined no code-editor

---
+++ src/app/components/button/button-outlined/button-outlined.module.ts
@@ -0,0 +1,10 @@
+import { ButtonOutlinedComponent } from './button-outlined.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [ButtonOutlinedComponent],
+  imports: [CommonModule],
+  exports: [ButtonOutlinedComponent],
+})
+export class ButtonOutlinedModule {}
--- src/app/components/code-editor/code-editor.module.ts
+++ src/app/components/code-editor/code-editor.module.ts
@@ -1,3 +1,4 @@
+import { ButtonOutlinedModule } from './../button/button-outlined/button-outlined.module';
 import { ButtonFilledModule } from './../button/button-filled/button-filled.module';
 import { CodeEditorComponent } from './code-editor.component';
 import { NgModule } from '@angular/core';
@@ -5,7 +6,7 @@ import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [CodeEditorComponent],
-  imports: [CommonModule, ButtonFilledModule],
+  imports: [CommonModule, ButtonFilledModule, ButtonOutlinedModule],
   exports: [CodeEditorComponent],
 })
 export class CodeEditorModule {}
